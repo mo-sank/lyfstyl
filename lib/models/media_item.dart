0cf9e0f91744d6be8e8189aa113f44dc65c42f25
camikrugel <112404317+camikrugel@users.noreply.github.com>
2025-09-30 12:00:42 -0500

    Subclasses started

--- lib/models/media_item.dart
+++ lib/models/media_item.dart
@@ -1,6 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-enum MediaType { film, book, music }
+enum MediaType { film, book, show, album, song }
 
 enum MediaSource { manual, letterboxd, goodreads, spotify, other }
 
@@ -51,20 +51,28 @@ class MediaItem {
 
   factory MediaItem.fromDoc(DocumentSnapshot<Map<String, dynamic>> doc) {
     final data = doc.data() ?? {};
-    return MediaItem(
-      mediaId: doc.id,
-      type: _parseMediaType(data['type'] as String?),
-      source: _parseMediaSource(data['source'] as String?),
-      title: (data['title'] ?? '') as String,
-      subtitle: data['subtitle'] as String?,
-      creator: data['creator'] as String?,
-      releaseDate: (data['releaseDate'] as Timestamp?)?.toDate(),
-      genres: (data['genres'] as List<dynamic>? ?? []).cast<String>(),
-      coverUrl: data['coverUrl'] as String?,
-      externalIds: (data['externalIds'] as Map<String, dynamic>? ?? {}),
-      createdAt: (data['createdAt'] as Timestamp?)?.toDate() ?? DateTime.now(),
-      updatedAt: (data['updatedAt'] as Timestamp?)?.toDate() ?? DateTime.now(),
-    );
+    final type= _parseMediaType(data['type'] as String?);
+    switch (type) {
+      case MediaType.book:
+        return BookItem.fromDoc(doc);
+      case MediaType.film:
+        return FilmItem.fromDoc(doc);
+      default:
+        return MediaItem(
+          mediaId: doc.id,
+          type: type,
+          source: _parseMediaSource(data['source'] as String?),
+          title: (data['title'] ?? '') as String,
+          subtitle: data['subtitle'] as String?,
+          creator: data['creator'] as String?,
+          releaseDate: (data['releaseDate'] as Timestamp?)?.toDate(),
+          genres: (data['genres'] as List<dynamic>? ?? []).cast<String>(),
+          coverUrl: data['coverUrl'] as String?,
+          externalIds: (data['externalIds'] as Map<String, dynamic>? ?? {}),
+          createdAt: (data['createdAt'] as Timestamp?)?.toDate() ?? DateTime.now(),
+          updatedAt: (data['updatedAt'] as Timestamp?)?.toDate() ?? DateTime.now(),
+        );
+    }
   }
 
   static MediaType _parseMediaType(String? value) {
@@ -73,8 +81,12 @@ class MediaItem {
         return MediaType.film;
       case 'book':
         return MediaType.book;
-      case 'music':
-        return MediaType.music;
+      case 'album':
+        return MediaType.album;
+      case 'song':
+        return MediaType.song;
+      case 'show':
+        return MediaType.show;
       default:
         return MediaType.film;
     }
@@ -93,5 +105,97 @@ class MediaItem {
       default:
         return MediaSource.other;
     }
+  }
+}
+// SUBCLASSES STARTED
+class FilmItem extends MediaItem {
+  final String? director;
+
+  FilmItem({
+    required super.mediaId,
+    required super.title,
+    required super.createdAt,
+    required super.updatedAt,
+    required super.source,
+    super.subtitle,
+    super.creator,
+    super.releaseDate,
+    super.genres,
+    super.coverUrl,
+    super.externalIds,
+    this.director,
+  }) : super(
+    type: MediaType.film,
+  );
+
+   @override
+  Map<String, dynamic> toMap() {
+    final map = super.toMap();
+    map['director'] = director;
+    return map;
+}
+
+factory FilmItem.fromDoc(DocumentSnapshot<Map<String, dynamic>> doc) {
+    final data = doc.data() ?? {};
+    return FilmItem(
+      mediaId: doc.id,
+      title: data['title'] ?? '',
+      source: MediaItem._parseMediaSource(data['source'] as String?),
+      createdAt: (data['createdAt'] as Timestamp?)?.toDate() ?? DateTime.now(),
+      updatedAt: (data['updatedAt'] as Timestamp?)?.toDate() ?? DateTime.now(),
+      subtitle: data['subtitle'] as String?,
+      creator: data['creator'] as String?,
+      releaseDate: (data['releaseDate'] as Timestamp?)?.toDate(),
+      genres: (data['genres'] as List<dynamic>? ?? []).cast<String>(),
+      coverUrl: data['coverUrl'] as String?,
+      externalIds: (data['externalIds'] as Map<String, dynamic>? ?? {}),
+      director: data['director'] as String?,
+    );
+  }
+}
+
+class BookItem extends MediaItem {
+  final int? pages;
+
+  BookItem({
+    required super.mediaId,
+    required super.title,
+    required super.createdAt,
+    required super.updatedAt,
+    required super.source,
+    super.subtitle,
+    super.creator,
+    super.releaseDate,
+    super.genres,
+    super.coverUrl,
+    super.externalIds,
+    this.pages,
+  }) : super(
+    type: MediaType.book,
+  );
+
+   @override
+  Map<String, dynamic> toMap() {
+    final map = super.toMap();
+    map['pages'] = pages;
+    return map;
+}
+
+factory BookItem.fromDoc(DocumentSnapshot<Map<String, dynamic>> doc) {
+    final data = doc.data() ?? {};
+    return BookItem(
+      mediaId: doc.id,
+      title: data['title'] ?? '',
+      source: MediaItem._parseMediaSource(data['source'] as String?),
+      createdAt: (data['createdAt'] as Timestamp?)?.toDate() ?? DateTime.now(),
+      updatedAt: (data['updatedAt'] as Timestamp?)?.toDate() ?? DateTime.now(),
+      subtitle: data['subtitle'] as String?,
+      creator: data['creator'] as String?,
+      releaseDate: (data['releaseDate'] as Timestamp?)?.toDate(),
+      genres: (data['genres'] as List<dynamic>? ?? []).cast<String>(),
+      coverUrl: data['coverUrl'] as String?,
+      externalIds: (data['externalIds'] as Map<String, dynamic>? ?? {}),
+      pages: data['pages'] as int?,
+    );
   }
 }
