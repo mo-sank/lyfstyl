e0ab7ff56c52ede51c538198c0afb0dbcffa2681
mo-sank <mohamedsankari3@gmail.com>
2025-10-14 20:34:43 -0500

    Rebuild Flutter web with enhanced genre browsing

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -24,14 +24,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "8d0b944ffc7ea927f655105c08607056
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "0af62239dc687b545f6ab79c4df3c70e",
+"flutter_bootstrap.js": "4c4b5e1dcbc6aa731f459e6833f61a3a",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "778017e2148c6b917378f69a5afbeaa3",
 "/": "778017e2148c6b917378f69a5afbeaa3",
-"main.dart.js": "998ca8759d50e904c8a018090f8c5e59",
+"main.dart.js": "8a9ede83ec1ae5344ed297cc0e91fb52",
 "manifest.json": "61592a44cd184aa00603cf2a181cf651",
 "version.json": "fa9ab6d19a17389f3a2c5117f19ee27e"};
 // The application shell files that are downloaded before a service worker can
