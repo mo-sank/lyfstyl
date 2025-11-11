87b7d17f42580b8682bf0499effe38eafec3430f
Julia <julia.p.zhang@vanderbilt.edu>
2025-11-10 21:51:16 -0600

    deploying changes

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -27,14 +27,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "8d0b944ffc7ea927f655105c08607056
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "4e469851d5599a104cc3209fcc942c23",
+"flutter_bootstrap.js": "23fc3e465b38ee7029a64ce813dcacd3",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "778017e2148c6b917378f69a5afbeaa3",
 "/": "778017e2148c6b917378f69a5afbeaa3",
-"main.dart.js": "da17d36fccea9576dbf156db305d3bb1",
+"main.dart.js": "c0015713e1321825e744078cb27619a9",
 "manifest.json": "61592a44cd184aa00603cf2a181cf651",
 "version.json": "fa9ab6d19a17389f3a2c5117f19ee27e"};
 // The application shell files that are downloaded before a service worker can
